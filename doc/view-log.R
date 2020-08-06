7feb80c65f1f4d795061b390d11d5a8054dd4893
Meera Patel <patelm9@mskcc.org>
2020-08-06 19:53:31 -0400

    add loadLog function

---
+++ doc/view-log.R
@@ -0,0 +1,16 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(setupAthena)
+library(tidyverse)
+library(rlang)
+
+## ----view log-----------------------------------------------------------------
+
+setupAthena::loadLog()
+
+
