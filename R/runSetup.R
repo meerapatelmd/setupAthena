68f9a20fe16fd7e398ca540a8e115310e8315859
Meera Patel <patelmeeray@gmail.com>
2020-08-10 22:49:21 -0400

    bug fix runSetup function

--- R/runSetup.R
+++ R/runSetup.R
@@ -1,18 +1,39 @@
-#' Run Full Setup
+#' @title Run Full Setup
 #' @description
 #' This function runs the entire process of dropping the target schema if it exists and populating the vocabulary tables.
-#' @import secretary
-#' @import pg13
+#' @param conn PARAM_DESCRIPTION, Default: conn
+#' @param targetSchema PARAM_DESCRIPTION, Default: 'public'
+#' @param cascade PARAM_DESCRIPTION, Default: TRUE
+#' @param vocabularyPath PARAM_DESCRIPTION
+#' @param verbose PARAM_DESCRIPTION, Default: TRUE
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[secretary]{typewrite_note}},\code{\link[secretary]{press_enter}},\code{\link[secretary]{typewrite}}
+#'  \code{\link[pg13]{lsSchema}},\code{\link[pg13]{createSchema}}
+#'  \code{\link[crayon]{crayon}}
+#' @rdname runSetup
 #' @export
-
+#' @importFrom secretary typewrite_note press_enter typewrite
+#' @importFrom pg13 lsSchema createSchema
+#' @importFrom crayon italic
 
 runSetup <-
         function(conn = conn,
                  targetSchema = "public",
                  cascade = TRUE,
                  vocabularyPath,
-                 cpt4 = TRUE,
                  verbose = TRUE) {
+
+                if (missing(vocabularyPath)) {
+                        stop('vocabularyPath missing with no default')
+                }
 
 
                 secretary::typewrite_note("This process will take approx 30 to 45 minutes.")
@@ -24,7 +45,7 @@ runSetup <-
 
                                 # Dropping and creating new schema
                                 if (verbose) {
-                                        secretary::typewrite("Dropping and creating new ", crayon::italic(targetSchema), " schema...")
+                                        secretary::typewrite("Dropping and creating new", crayon::italic(targetSchema), "schema...")
                                 }
 
                                 clearSchema(conn = conn,
