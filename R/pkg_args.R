59a932ba97d7ee6bd386fb78ffb19747adc68baf
Meera Patel <patelmeeray@gmail.com>
2020-12-06 20:28:09 -0500

    update v0.2.0 docs

--- R/pkg_args.R
+++ R/pkg_args.R
@@ -3,8 +3,9 @@
 #'
 #' @param path_to_csvs  Path to folder containing the unpacked vocabularies as csv files, downloaded as a zip from \url{athena.ohdsi.org}.
 #' @param umls_api_key  UMLS API Key for CPT4 reconstitution.
-#' @inheritParams pg13::query
-#' @inheritParams pg13::send
+#' @param conn A Postgres connection object from the DatabaseConnector package.
+#' @param verbose If TRUE, prints back activity in the R console as it is occurring.
+#' @param render_sql If TRUE, the SQL statement is printed back in the R console for reference.
 #' @param target_schema Schema where the OMOP Vocabulary ('Athena') Tables will be written to.
 #' @param steps Options include c("prepare_cpt4", "drop_tables","copy", "indices", "constraints", "log"). Each step corresponds to the function used and may be independently executed. \code{\link{prepare_cpt4}} is not included in default because of its tendency to run slow and the java script used to reconstitute the CPT4 needs to run uninterrupted or else the resulting CONCEPT.csv file will be damaged. Therefore, it is advised to reconstitute CPT4 separately from the Command Line or using the aforementioned function in this package though the option to do it as part of \code{\link{run_setup}} is available.
 #'
