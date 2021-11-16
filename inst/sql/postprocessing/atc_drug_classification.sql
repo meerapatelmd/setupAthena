2ccf822fe035818f9191e040a30412a1decb8f3e
Meera <mpatel@concertai.com>
2021-11-15 19:24:08 -0500

    change `ingredient_` fields to `in_pin_` to capture true semantic meaning

--- inst/sql/postprocessing/atc_drug_classification.sql
+++ inst/sql/postprocessing/atc_drug_classification.sql
@@ -7,148 +7,75 @@
 / - Only valid relationsihps are included
 / - Both invalid and valid RxNorm Ingredients are given
 / - RxNorm 'Precise Ingredient' concepts are also included
-/   in the `ingredient_*` fields
+/   in the `in_pin_*` fields
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
- DROP TABLE IF EXISTS terminology_log.tmp_terminology;
- CREATE TABLE terminology_log.tmp_terminology (
-   start_datetime timestamp without time zone,
-   end_datetime timestamp without time zone,
-   table_name varchar(60) NOT NULL,
-   source_system varchar(100),
-   source_version varchar(100),
-   jira_ticket varchar(10)
-);
 
- DROP TABLE IF EXISTS terminology_log.tmp_terminology2;
- CREATE TABLE terminology_log.tmp_terminology2 (
-   start_datetime timestamp without time zone,
-   end_datetime timestamp without time zone,
-   table_name varchar(60) NOT NULL,
-   source_system varchar(100),
-   source_version varchar(100),
-   jira_ticket varchar(10)
-);
-
-
- CREATE TABLE IF NOT EXISTS terminology_log.terminology (
-   start_datetime timestamp without time zone,
-   end_datetime timestamp without time zone,
-   table_name varchar(60),
-   source_system varchar(100),
-   source_version varchar(100),
-   jira_ticket varchar(10)
-);
-
-
-
-CREATE OR REPLACE PROCEDURE log_start()
-AS $$
-DECLARE
-  start_datetime timestamp without time zone := date_trunc('second', timeofday()::timestamp);
-  table_name varchar(60)    := 'atc_classification';
-  source_system varchar(25) := 'omop';
-  source_version varchar(25);
-  jira_ticket varchar(10) := 'INF-222';
-BEGIN
-  SELECT sa_release_version
-    into source_version
-    from terminology_log.omop_athena oa
-    where oa.transfer_end_datetime  in (
-    select max(transfer_end_datetime)
-    from terminology_log.omop_athena
-    where transfer_end_datetime is not null);
-
-
-EXECUTE
-  'INSERT INTO terminology_log.tmp_terminology(start_datetime, table_name, source_system, source_version, jira_ticket) ' ||
-  'VALUES ' ||
-  '  (    ' ||
-  '   ''' || start_datetime || '''' || ', ' ||
-  '   ''' || table_name      || '''' || ', ' ||
-  '   ''' || source_system   || '''' || ', ' ||
-  '   ''' || source_version  || '''' || ', ' ||
-  '   ''' || jira_ticket     || '''' ||
-  '  );';
-
- commit;
-
-END;
-$$
-LANGUAGE plpgsql;
-
-
-call log_start();
-
-
-
-
-
-drop table if exists terminology.tmp_atc_classification1;
-create table terminology.tmp_atc_classification1 (
+drop table if exists omop_classification.tmp_atc_classification1;
+create table omop_classification.tmp_atc_classification1 (
  atc_1st_id bigint,
- atc_1st_code varchar(max),
- atc_1st_name varchar(max),
+ atc_1st_code text,
+ atc_1st_name text,
  atc_2nd_id bigint,
- atc_2nd_code varchar(max),
- atc_2nd_name varchar(max),
+ atc_2nd_code text,
+ atc_2nd_name text,
  atc_3rd_id bigint,
- atc_3rd_code varchar(max),
- atc_3rd_name varchar(max),
+ atc_3rd_code text,
+ atc_3rd_name text,
  atc_4th_id bigint,
- atc_4th_code varchar(max),
- atc_4th_name varchar(max),
+ atc_4th_code text,
+ atc_4th_name text,
  atc_5th_id bigint,
- atc_5th_code varchar(max),
- atc_5th_name varchar(max),
- ingredient_id bigint,
- ingredient_code varchar(max),
- ingredient_name varchar(max)
+ atc_5th_code text,
+ atc_5th_name text,
+ in_pin_id bigint,
+ in_pin_code text,
+ in_pin_name text
 )
 ;
 
-drop table if exists terminology.tmp_atc_classification2;
-create table terminology.tmp_atc_classification2 (
+drop table if exists omop_classification.tmp_atc_classification2;
+create table omop_classification.tmp_atc_classification2 (
  atc_1st_id bigint,
- atc_1st_code varchar(max),
- atc_1st_name varchar(max),
+ atc_1st_code text,
+ atc_1st_name text,
  atc_2nd_id bigint,
- atc_2nd_code varchar(max),
- atc_2nd_name varchar(max),
+ atc_2nd_code text,
+ atc_2nd_name text,
  atc_3rd_id bigint,
- atc_3rd_code varchar(max),
- atc_3rd_name varchar(max),
+ atc_3rd_code text,
+ atc_3rd_name text,
  atc_4th_id bigint,
- atc_4th_code varchar(max),
- atc_4th_name varchar(max),
+ atc_4th_code text,
+ atc_4th_name text,
  atc_5th_id bigint,
- atc_5th_code varchar(max),
- atc_5th_name varchar(max),
- ingredient_id bigint,
- ingredient_code varchar(max),
- ingredient_name varchar(max)
+ atc_5th_code text,
+ atc_5th_name text,
+ in_pin_id bigint,
+ in_pin_code text,
+ in_pin_name text
 )
 ;
 
-drop table if exists terminology.atc_classification;
-create table terminology.atc_classification (
+drop table if exists omop_classification.atc_classification;
+create table omop_classification.atc_classification (
  atc_1st_id bigint,
- atc_1st_code varchar(max),
- atc_1st_name varchar(max),
+ atc_1st_code text,
+ atc_1st_name text,
  atc_2nd_id bigint,
- atc_2nd_code varchar(max),
- atc_2nd_name varchar(max),
+ atc_2nd_code text,
+ atc_2nd_name text,
  atc_3rd_id bigint,
- atc_3rd_code varchar(max),
- atc_3rd_name varchar(max),
+ atc_3rd_code text,
+ atc_3rd_name text,
  atc_4th_id bigint,
- atc_4th_code varchar(max),
- atc_4th_name varchar(max),
+ atc_4th_code text,
+ atc_4th_name text,
  atc_5th_id bigint,
- atc_5th_code varchar(max),
- atc_5th_name varchar(max),
- ingredient_id bigint,
- ingredient_code varchar(max),
- ingredient_name varchar(max)
+ atc_5th_code text,
+ atc_5th_name text,
+ in_pin_id bigint,
+ in_pin_code text,
+ in_pin_name text
 )
 ;
 
@@ -161,14 +88,14 @@ SELECT DISTINCT
   c2.concept_id AS atc_2nd_id,
   c2.concept_code AS atc_2nd_code,
   c2.concept_name AS atc_2nd_name
-  FROM omop_athena.concept c2
+  FROM omop_vocabulary.concept c2
   INNER JOIN (
     SELECT *
-    FROM omop_athena.concept_relationship
+    FROM omop_vocabulary.concept_relationship
     WHERE relationship_id = 'Subsumes' AND invalid_reason IS NULL) cr
   ON cr.concept_id_2 = c2.concept_id
   INNER JOIN (
-    SELECT * FROM omop_athena.concept
+    SELECT * FROM omop_vocabulary.concept
     WHERE
       vocabulary_id = 'ATC' AND
       concept_class_id = 'ATC 1st' ) c1
@@ -188,10 +115,10 @@ SELECT DISTINCT
   c2.concept_id AS atc_3rd_id,
   c2.concept_code AS atc_3rd_code,
   c2.concept_name AS atc_3rd_name
-FROM omop_athena.concept c2
-INNER JOIN (SELECT * FROM omop_athena.concept_relationship WHERE relationship_id = 'Subsumes' AND invalid_reason is null) cr
+FROM omop_vocabulary.concept c2
+INNER JOIN (SELECT * FROM omop_vocabulary.concept_relationship WHERE relationship_id = 'Subsumes' AND invalid_reason is null) cr
 ON cr.concept_id_2 = c2.concept_id
-INNER JOIN (SELECT * FROM omop_athena.concept WHERE vocabulary_id = 'ATC' AND concept_class_id = 'ATC 2nd' AND invalid_reason is null) c1
+INNER JOIN (SELECT * FROM omop_vocabulary.concept WHERE vocabulary_id = 'ATC' AND concept_class_id = 'ATC 2nd' AND invalid_reason is null) c1
 ON cr.concept_id_1 = c1.concept_id
 WHERE
  c2.vocabulary_id = 'ATC' AND
@@ -208,16 +135,16 @@ SELECT DISTINCT
   c2.concept_id AS atc_4th_id,
   c2.concept_code AS atc_4th_code,
   c2.concept_name AS atc_4th_name
-FROM omop_athena.concept c2
+FROM omop_vocabulary.concept c2
 INNER JOIN
   (SELECT *
-    FROM omop_athena.concept_relationship
+    FROM omop_vocabulary.concept_relationship
     WHERE relationship_id = 'Subsumes'
     AND invalid_reason is null) cr
 ON cr.concept_id_2 = c2.concept_id
 INNER JOIN
   (SELECT *
-    FROM omop_athena.concept
+    FROM omop_vocabulary.concept
     WHERE vocabulary_id = 'ATC'
     AND concept_class_id = 'ATC 3rd'
     AND invalid_reason is null) c1
@@ -237,10 +164,10 @@ SELECT DISTINCT
   c2.concept_id AS atc_5th_id,
   c2.concept_code AS atc_5th_code,
   c2.concept_name AS atc_5th_name
-FROM omop_athena.concept c2
-INNER JOIN (SELECT * FROM omop_athena.concept_relationship WHERE relationship_id = 'Subsumes' AND invalid_reason is null) cr
+FROM omop_vocabulary.concept c2
+INNER JOIN (SELECT * FROM omop_vocabulary.concept_relationship WHERE relationship_id = 'Subsumes' AND invalid_reason is null) cr
 ON cr.concept_id_2 = c2.concept_id
-INNER JOIN (SELECT * FROM omop_athena.concept WHERE vocabulary_id = 'ATC' AND concept_class_id = 'ATC 4th' AND invalid_reason is null) c1
+INNER JOIN (SELECT * FROM omop_vocabulary.concept WHERE vocabulary_id = 'ATC' AND concept_class_id = 'ATC 4th' AND invalid_reason is null) c1
 ON cr.concept_id_1 = c1.concept_id
 WHERE
  c2.vocabulary_id = 'ATC' AND
@@ -248,7 +175,7 @@ WHERE
  c2.invalid_reason is null
 );
 
-insert into terminology.tmp_atc_classification1
+insert into omop_classification.tmp_atc_classification1
 (
 SELECT DISTINCT
  a.atc_1st_id,
@@ -266,9 +193,9 @@ SELECT DISTINCT
  d.atc_5th_id,
  d.atc_5th_code,
  d.atc_5th_name,
-c2.concept_id AS ingredient_id,
-c2.concept_code AS ingredient_code,
-c2.concept_name AS ingredient_name
+c2.concept_id AS in_pin_id,
+c2.concept_code AS in_pin_code,
+c2.concept_name AS in_pin_name
 from part_a a
 LEFT JOIN part_b b
 ON a.atc_2nd_id = b.atc_2nd_id
@@ -277,21 +204,21 @@ ON b.atc_3rd_id = c.atc_3rd_id
 LEFT JOIN part_d d
 ON c.atc_4th_id = d.atc_4th_id
 INNER JOIN
-    (SELECT * FROM omop_athena.concept_relationship WHERE relationship_id = 'ATC - RxNorm pr lat' AND invalid_reason is null) cr
+    (SELECT * FROM omop_vocabulary.concept_relationship WHERE relationship_id = 'ATC - RxNorm pr lat' AND invalid_reason is null) cr
 ON cr.concept_id_1 = d.atc_5th_id
-INNER JOIN (SELECT * FROM omop_athena.concept WHERE vocabulary_id = 'RxNorm' AND concept_class_id = 'Ingredient' AND invalid_reason is null) c2
+INNER JOIN (SELECT * FROM omop_vocabulary.concept WHERE vocabulary_id = 'RxNorm' AND concept_class_id = 'Ingredient' AND invalid_reason is null) c2
   ON cr.concept_id_2 = c2.concept_id
 INNER JOIN (
   SELECT DISTINCT
-    c1.concept_id AS ingredient_id,
+    c1.concept_id AS in_pin_id,
     c2.concept_id AS rxnorm_id,
     c2.concept_code AS rxnorm_code,
     c2.concept_name AS rxnorm_name,
     c2.concept_class_id AS rxnorm_class
-  FROM omop_athena.concept c1
-  INNER JOIN omop_athena.concept_ancestor ca
+  FROM omop_vocabulary.concept c1
+  INNER JOIN omop_vocabulary.concept_ancestor ca
   ON ca.ancestor_concept_id = c1.concept_id
-  INNER JOIN (SELECT * FROM omop_athena.concept WHERE vocabulary_id = 'RxNorm' AND invalid_reason is null) c2
+  INNER JOIN (SELECT * FROM omop_vocabulary.concept WHERE vocabulary_id = 'RxNorm' AND invalid_reason is null) c2
   ON ca.descendant_concept_id = c2.concept_id
   WHERE
    c1.vocabulary_id = 'RxNorm' AND
@@ -300,26 +227,26 @@ INNER JOIN (
    c1.concept_id <> c2.concept_id
   UNION
   SELECT DISTINCT
-    c1.concept_id AS ingredient_id,
+    c1.concept_id AS in_pin_id,
     c2.concept_id AS rxnorm_id,
     c2.concept_code AS rxnorm_code,
     c2.concept_name AS rxnorm_name,
     c2.concept_class_id AS rxnorm_class
-  FROM omop_athena.concept c1
-  INNER JOIN omop_athena.concept_ancestor ca
+  FROM omop_vocabulary.concept c1
+  INNER JOIN omop_vocabulary.concept_ancestor ca
   ON ca.descendant_concept_id = c1.concept_id
-  INNER JOIN (SELECT * FROM omop_athena.concept WHERE vocabulary_id = 'RxNorm' AND invalid_reason is null) c2
+  INNER JOIN (SELECT * FROM omop_vocabulary.concept WHERE vocabulary_id = 'RxNorm' AND invalid_reason is null) c2
   ON ca.ancestor_concept_id = c2.concept_id
   WHERE
    c1.vocabulary_id = 'RxNorm' AND
    c1.concept_class_id = 'Ingredient' AND
    c1.invalid_reason is null AND
    c1.concept_id <> c2.concept_id) ca
- ON ca.ingredient_id = c2.concept_id
+ ON ca.in_pin_id = c2.concept_id
 );
 
 -- Traverse the RxNorm Precise Ingredient 'Form of' relationship to include Precise Ingredients
-INSERT INTO terminology.tmp_atc_classification2
+INSERT INTO omop_classification.tmp_atc_classification2
 SELECT DISTINCT
  tmp1.atc_1st_id,
  tmp1.atc_1st_code,
@@ -336,13 +263,13 @@ SELECT DISTINCT
  tmp1.atc_5th_id,
  tmp1.atc_5th_code,
  tmp1.atc_5th_name,
- pin.concept_id AS ingredient_id,
- pin.concept_code AS ingredient_code,
- pin.concept_name AS ingredient_name
-FROM terminology.tmp_atc_classification1 tmp1
-LEFT JOIN omop_athena.concept_relationship cr
-ON cr.concept_id_2 = tmp1.ingredient_id
-INNER JOIN omop_athena.concept pin
+ pin.concept_id AS in_pin_id,
+ pin.concept_code AS in_pin_code,
+ pin.concept_name AS in_pin_name
+FROM omop_classification.tmp_atc_classification1 tmp1
+LEFT JOIN omop_vocabulary.concept_relationship cr
+ON cr.concept_id_2 = tmp1.in_pin_id
+INNER JOIN omop_vocabulary.concept pin
 ON pin.concept_id = cr.concept_id_1
 WHERE
   cr.invalid_reason IS NULL AND
@@ -351,69 +278,12 @@ WHERE
 
 
 -- Write final table
-INSERT INTO terminology.atc_classification
-SELECT * FROM terminology.tmp_atc_classification1
+INSERT INTO omop_classification.atc_classification
+SELECT * FROM omop_classification.tmp_atc_classification1
 UNION
-SELECT * FROM terminology.tmp_atc_classification2
+SELECT * FROM omop_classification.tmp_atc_classification2
 ;
 
-commit;
-
-CREATE OR REPLACE PROCEDURE log_end()
-AS $$
-DECLARE
-  start_datetime2 timestamp without time zone;
-  end_datetime2 timestamp without time zone := date_trunc('second', timeofday()::timestamp);
-  table_name2 varchar(60);
-  source_system2 varchar(25);
-  source_version2 varchar(25);
-  jira_ticket2 varchar(10);
-BEGIN
-  SELECT start_datetime
-  INTO start_datetime2
-  FROM terminology_log.tmp_terminology;
-
-  SELECT table_name
-  INTO table_name2
-  FROM terminology_log.tmp_terminology;
-
-  SELECT source_system
-  INTO source_system2
-  FROM terminology_log.tmp_terminology;
-
-  SELECT source_version
-  INTO source_version2
-  FROM terminology_log.tmp_terminology;
-
-  SELECT jira_ticket
-  INTO jira_ticket2
-  FROM terminology_log.tmp_terminology;
-
-EXECUTE
-  'INSERT INTO terminology_log.tmp_terminology2 ' ||
-  'VALUES ' ||
-  '  (    ' ||
-  '   ''' || start_datetime2  || '''' || ', ' ||
-  '   ''' || end_datetime2    || '''' || ', ' ||
-  '   ''' || table_name2      || '''' || ', ' ||
-  '   ''' || source_system2   || '''' || ', ' ||
-  '   ''' || source_version2  || '''' || ', ' ||
-  '   ''' || jira_ticket2     || '''' ||
-  '  );';
-
- insert into terminology_log.terminology
- select * from terminology_log.tmp_terminology2;
-
-END;
-$$
-LANGUAGE plpgsql;
-
-
-call log_end();
-
-
-DROP table if EXISTS terminology_log.tmp_terminology;
-DROP table if EXISTS terminology_log.tmp_terminology2;
-DROP TABLE terminology.tmp_atc_classification1;
-DROP TABLE terminology.tmp_atc_classification2;
+DROP TABLE omop_classification.tmp_atc_classification1;
+DROP TABLE omop_classification.tmp_atc_classification2;
 
